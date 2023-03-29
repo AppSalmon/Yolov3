cd94d03ca2e8a8fe2112d028933273f4e0cb2531
AppSalmon <hoangtuan.salmon@gmail.com>
2023-03-29 13:51:17 +0700

    Test commit

---
+++ yolo_object_detection.py
@@ -0,0 +1,94 @@
+import cv2
+import numpy as np
+import glob
+import random
+import matplotlib.pyplot as plt
+
+def show_pic(img):
+    # image = cv2.imread(img)
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.figure(figsize=(6,6))
+    plt.title('Kết quả')
+    plt.imshow(image)
+    plt.show()
+
+# Load Yolo
+net = cv2.dnn.readNet("yolov3_training_last_dataset_qxn_1_hour.weights", "yolov3_testing.cfg")
+
+# Name custom object
+classes = ["Woman"]
+
+# Images path
+images_path = glob.glob(r"data_test\*.png")
+
+
+layer_names = net.getLayerNames()
+
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Insert here the path of your images
+random.shuffle(images_path)
+print(images_path)
+# loop through all the images
+for img_path in images_path:
+    # Loading image
+    img = cv2.imread(img_path)
+    print(type(img))
+    if img is None:
+        continue
+    img = cv2.resize(img, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.3:
+                # Object detected
+                print(class_id)
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    print(indexes)
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[class_ids[i]]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img, label, (x, y + 30), font, 3, color, 2)
+
+
+    show_pic(img)
+    key = cv2.waitKey(0)
+    
+# Show picture by cv2 
+    
+    # cv2.imshow("Image", img)
+    # print(img)
+    # break
+# cv2.destroyAllWindows()
